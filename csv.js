872470590ffb8eabc783823fd4c6390223253a34
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-03-27 12:06:24 +0530

    changes are done

--- csv.js
+++ csv.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 const ObjectsToCsv = require("objects-to-csv");
 
 // If you use "await", code must be inside an asynchronous function:
@@ -13,30 +11,6 @@ const convertJSONtoCSV = async (arr) => {
   } catch (e) {
     console.log(e);
   }
-  // let csvContent = arr
-  //   .map((element) => {
-  //     return Object.values(element)
-  //       .map((item) => `"${item}"`)
-  //       .join(",");
-  //   })
-  //   .join("\n");
-
-  // fs.writeFile(
-  //   "saved-beardo.csv",
-  //   "Title, Image, Link, Price,Maxretailprice, Reviews, Stars, Total Reviews and  Ratings, Positive Reviews and ratings, Negative Reviews and Ratings, 5star Reviews and Ratings, 4star Reviews and ratings, 3starReviews and Ratings, 2starReviews and Ratings, 1starReviews and ratings" +
-  //     "\n" +
-  //     csvContent,
-  //   "utf8",
-  //   function (err) {
-  //     if (err) {
-  //       console.log(
-  //         "Some error occurred - file either not saved or corrupted."
-  //       );
-  //     } else {
-  //       console.log("File has been saved!");
-  //     }
-  //   }
-  // );
 };
 
 module.exports = { convertJSONtoCSV };
