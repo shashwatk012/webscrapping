df4cdfdd96c631eabd9b4573c6782209f4483c4a
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-05-22 23:41:01 +0530

    sds

--- text.js
+++ text.js
@@ -1,15 +1,25 @@
 "use strict";
 
 const headers = {
-  "User-Agent":
-    "Mozilla/5.0 (Linux; Android 11; SAMSUNG SM-G973U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36",
-  Accept:
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-  "Accept-Language": "en-US,en;q=0.9,la;q=0.8",
-  "Accept-Encoding": "gzip, deflate, br",
-  Referer: "https://www.google.com/",
-  "Cache-Control": "no-cache",
-  Connection: "keep-alive",
+  headers: {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9,la;q=0.8",
+    Host: "httpbin.org",
+    "Sec-Ch-Ua":
+      '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+    "X-Amzn-Trace-Id": "Root=1-646baec9-23c65be55fbb54967e9160ef",
+  },
 };
 const allProducts =
   "div._1YokD2._2GoDe3>div._1YokD2._3Mn1Gg>div._1AtVbE.col-12-12>div._13oc-S>div";
