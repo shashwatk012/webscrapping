b01cc6348d9473575780dcb8ca61a7127da7cdd8
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-05-22 21:15:34 +0530

    dvf

--- text.js
+++ text.js
@@ -4,8 +4,6 @@ const headers = {
   headers: {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "X-User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 FKUA/website/42/website/Desktop",
     Accept:
       "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.5",
