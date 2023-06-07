33454503a11595304e1be59ceb49bdc551982adc
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-07 14:01:29 +0530

    added more routes

---
+++ flipkart3/flipkarturlDetails.js
@@ -0,0 +1,61 @@
+"use strict";
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { headers, allProducts, imglink, replce } = require("../text");
+
+const flipkartfetchUrlDetails = async (url) => {
+  try {
+    const response = await axios.get(url, headers);
+
+    const html = response.data;
+
+    const $ = cheerio.load(html);
+
+    const beards = [];
+
+    $(allProducts).each(async (_idx, el) => {
+      // selecting the elements to be scrapped
+      const beardo = $(el);
+      let imagelink = beardo.find(imglink).attr("src"); // scraping the image
+      if (!imagelink) {
+        imagelink = beardo.find("img._2r_T1I").attr("src"); // scraping the image
+      }
+
+      const link = beardo // scraping the link of the product
+        .find("a")
+        .attr("href");
+
+      let price = beardo.find("div._30jeq3").text();
+      price = replce(price);
+
+      let maxretailprice = beardo.find("div._3I9_wc").text();
+
+      if (maxretailprice === "") {
+        maxretailprice = price;
+      } else {
+        maxretailprice = replce(maxretailprice);
+      }
+
+      let ads = beardo.find("div._2tfzpE>span").text();
+      if (ads === "Ad") {
+        ads = "Yes";
+      } else {
+        ads = "No";
+      }
+
+      let element = {
+        imagelink,
+        productlink: `https://www.flipkart.com${link}`,
+        price,
+        maxretailprice,
+        IsAds: ads,
+      };
+      beards.push(element); //storing the details in an array
+    });
+    return beards;
+  } catch (error) {
+    return [{ message: "Can not fetch" }];
+  }
+};
+
+module.exports = { flipkartfetchUrlDetails };
