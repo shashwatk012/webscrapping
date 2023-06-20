fc60d06f70ae6d699071bd4ec7baac0c80f4c822
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-20 17:07:22 +0530

    added the port

--- app.js
+++ app.js
@@ -4,13 +4,20 @@
 require("events").EventEmitter.defaultMaxListeners = Infinity;
 const express = require("express");
 const app = express();
-const path = require("path");
 const { router } = require("./router");
 
 app.use(router); //Creating router to make main file neat and clean
 
+app.listen(8000, () => {
+  console.log(`Server running at ${8000}`);
+});
+
 app.listen(80, () => {
   console.log(`Server running at ${80}`);
+});
+
+app.listen(6000, () => {
+  console.log(`Server running at ${6000}`);
 });
 
 module.exports = app;
