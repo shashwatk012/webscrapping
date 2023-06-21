21f8bfcd7691268e03eb4357bd85c9b0d39c1016
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-21 08:56:25 +0530

    wde

--- Allamazonfiles/amazonbylink/amazondetails.js
+++ Allamazonfiles/amazonbylink/amazondetails.js
@@ -15,8 +15,6 @@ const amazonfetchIndividualDetails = async (url, browser, page) => {
 
     let html = await page.content();
 
-    console.log(html);
-
     let $ = cheerio.load(html);
 
     let captchalink = $("div.a-row.a-text-center>img").attr("src");
