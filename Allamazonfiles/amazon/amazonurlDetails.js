ad118bf59459f77a73ee6a368008b3f820b92b68
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-22 23:25:54 +0530

    added scrabing bee

--- Allamazonfiles/amazon/amazonurlDetails.js
+++ Allamazonfiles/amazon/amazonurlDetails.js
@@ -1,54 +1,70 @@
 const cheerio = require("cheerio");
 const amazontext = require("./amazontext");
+const scrapingbee = require("scrapingbee");
 
 const amazonfetchUrlDetails = async (url, browser, page) => {
   try {
-    page = await browser.browser.newPage();
+    var client = new scrapingbee.ScrapingBeeClient(
+      "I1VKMCIP31YMI7PKCT2R2WXO6D3UY5OW59GPK6IKOWYKAXIHLA585HLENJ0CZ51SFMTTYIGRAW7ONVZG"
+    );
+    var response = await client.get({
+      url: url,
+      params: {
+        // premium_proxy: "True",
+        block_ads: "True",
+        block_resources: "True",
+      },
+    });
 
-    await page.goto(url);
+    var decoder = new TextDecoder();
+    var html = decoder.decode(response.data);
+    // page = await browser.browser.newPage();
 
-    await page.waitForTimeout(1000);
+    // await page.authenticate();
+    // await page.goto("http://httpbin.org/ip");
 
-    let html = await page.content();
-    console.log(html.substring(1, 100));
+    // await page.waitForTimeout(1000);
+
+    // let html = await page.content();
+    // console.log(html);
     // await page.screenshot({
     //   path: "screenshot.jpg",
     // });
 
-    let $ = cheerio.load(html);
+    // let $ = cheerio.load(html);
 
-    let captchalink = $("div.a-row.a-text-center>img").attr("src");
+    // let captchalink = $("div.a-row.a-text-center>img").attr("src");
 
-    let captcha = undefined;
+    // let captcha = undefined;
 
-    if (captchalink) {
-      console.log(captchalink);
+    // if (captchalink) {
+    //   console.log(captchalink);
 
-      const readline = require("readline").createInterface({
-        input: process.stdin,
-        output: process.stdout,
-      });
+    //   const readline = require("readline").createInterface({
+    //     input: process.stdin,
+    //     output: process.stdout,
+    //   });
 
-      readline.question("Type the captcha", (name) => {
-        console.log(`Captcha is ${name}!`);
-        captcha = name;
-        readline.close();
-      });
+    //   readline.question("Type the captcha", (name) => {
+    //     console.log(`Captcha is ${name}!`);
+    //     captcha = name;
+    //     readline.close();
+    //   });
 
-      await page.waitForTimeout(20000);
+    //   await page.waitForTimeout(20000);
 
-      await page.type("input#captchacharacters", captcha);
+    //   await page.type("input#captchacharacters", captcha);
 
-      await page.click(
-        "span.a-button.a-button-primary.a-span12>span.a-button-inner>button.a-button-text"
-      );
+    //   await page.click(
+    //     "span.a-button.a-button-primary.a-span12>span.a-button-inner>button.a-button-text"
+    //   );
 
-      await page.waitForTimeout(5000);
-    }
+    //   await page.waitForTimeout(5000);
+    // }
 
-    html = await page.content();
+    // html = await page.content();
 
-    await page.close();
+    // await page.close();
 
     // cheerio nodejs module to load html
     $ = cheerio.load(html);
