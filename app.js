75bc8c052f105003b989ec8faa9a725d3f688be0
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-22 19:03:57 +0530

    changed the port

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ const { router } = require("./router");
 
 app.use(router); //Creating router to make main file neat and clean
 
-app.listen(8000, () => {
-  console.log(`Server running at ${8000}`);
-});
-
-// app.listen(80, () => {
-//   console.log(`Server running at ${80}`);
+// app.listen(8000, () => {
+//   console.log(`Server running at ${8000}`);
 // });
 
-app.listen(6000, () => {
-  console.log(`Server running at ${6000}`);
+app.listen(80, () => {
+  console.log(`Server running at ${80}`);
 });
 
 module.exports = app;
