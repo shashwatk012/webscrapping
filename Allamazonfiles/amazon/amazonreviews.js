cb61b83add8b442d453588f7f70557154e99be46
shashwatk012 <shashwatkesharwani.12@gmail.com>
2023-06-29 19:37:33 +0530

    ddb

--- Allamazonfiles/amazon/amazonreviews.js
+++ Allamazonfiles/amazon/amazonreviews.js
@@ -54,72 +54,20 @@ const amazonscrapreviews = (html) => {
   return obj;
 };
 
-// Add stealth plugin and use defaults (all tricks to hide puppeteer usage)
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
-puppeteer.use(StealthPlugin());
-
-const { executablePath } = require("puppeteer");
-
-// Add adblocker plugin to block all ads and trackers (saves bandwidth)
-const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
-puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
-
 const amazonfetchReviews = async (url) => {
   try {
     console.log(url);
-    let browser = await puppeteer.launch({
-      headless: `true`, // indicates that we want the browser visible
-      defaultViewport: false, // indicates not to use the default viewport size but to adjust to the user's screen resolution instead
-      // userDataDir: "./tmp", // caches previous actions for the website. Useful for remembering if we've had to solve captchas in the past so we don't have to resolve them
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      executablePath: executablePath(),
-    });
-
-    page = await browser.newPage();
-    await page.goto(url);
-
-    await page.waitForTimeout(1000);
-
-    let lastHeight = await page.evaluate("document.body.scrollHeight");
-
+    let html;
     while (true) {
-      await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
-      await page.waitForTimeout(1000); // sleep a bit
-      let newHeight = await page.evaluate("document.body.scrollHeight");
-      if (newHeight === lastHeight) {
+      html = await proxyReqforreviews(url);
+      if (html !== "") {
         break;
-      }
-      lastHeight = newHeight;
-    }
-
-    const html = await page.content();
-
-    await page.close();
-    await browser.close();
-
-    const $ = cheerio.load(html);
-
-    // Scraping the Global number of reviews
-    const noReviews = $(amazontext.A_NUM_REVIEWS_CN);
-    numberReviews = noReviews.html();
-    if (!numberReviews) {
-      while (true) {
-        html = await proxyReqforreviews(url);
-        if (html !== "") {
-          break;
-        }
       }
     }
 
     return amazonscrapreviews(html);
   } catch (error) {
     try {
-      if (page) {
-        await page.close();
-      }
-      if (browser) {
-        await browser.close();
-      }
       console.log("Some thing Went Wrong on review.js");
       let html;
       while (true) {
